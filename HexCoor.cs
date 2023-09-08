c634b1ad1143db2de0efd7f3ac510d77ea621e72
Martynas Janušaitis <martynasjanusaitis1@gmail.com>
2023-09-08 18:40:18 +0300

    Adding hex coor functionality
    Adding basic hex coordinate functionality
    Useful lookup for this system: https://www.redblobgames.com/grids/hexagons/

---
+++ HexCoor.cs
@@ -0,0 +1,30 @@
+﻿namespace Code_of_the_Hexagon_Server
+{
+    class HexCoor
+    {
+        // Kodas ir hex idėja paimta iš:
+        // https://www.redblobgames.com/grids/hexagons/
+        // Naudinga detalė skaičiavimams: q + r + s == 0
+        public int q { get; private set; }
+        public int r { get; private set; }
+        public int s { get; private set; }
+        public HexCoor(int Q, int R, int S)
+        {
+            (q, r, s) = (Q, R, S);
+        }
+        public HexCoor(int col, int row)
+        {
+            q = col - (row - (row & 1)) / 2;
+            r = row;
+            s = -q - r;
+        }
+        public int GetCol()
+        {
+            return q + (r - (r & 1)) / 2;
+        }
+        public int GetRow()
+        {
+            return r;
+        }
+    }
+}
